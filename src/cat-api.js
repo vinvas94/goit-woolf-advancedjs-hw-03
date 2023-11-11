bc4a10163659fffaf74f76260075571457a7bfa4
vinvas94 <vintoniv1111@gmail.com>
2023-11-11 20:09:19 +0100

    cambio api-key

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.thecatapi.com/v1';
 axios.defaults.headers.common['x-api-key'] =
-  'live_lYmOKTQRHsB8sJe9ygzuQe7FXcc2ZvtWiVnNozCi85ub5YUZiLk8YnrbwAneZI04';
+  'live_n4VWIaVJ8MEwjf6Kd5BbI1T5MOpXlUsfsYzkr7ScX1gYNGyuofj3slrEniHrovtb';
 
 export function fetchBreeds() {
   return axios.get(`${BASE_URL}/breeds`);
