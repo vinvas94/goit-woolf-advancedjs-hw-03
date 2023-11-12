af79d693d0faaac4db625edb63eeda6a4952a1ba
vinvas94 <vintoniv1111@gmail.com>
2023-11-12 14:00:22 +0100

    add async

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,10 +4,10 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 axios.defaults.headers.common['x-api-key'] =
   'live_n4VWIaVJ8MEwjf6Kd5BbI1T5MOpXlUsfsYzkr7ScX1gYNGyuofj3slrEniHrovtb';
 
-export function fetchBreeds() {
-  return axios.get(`${BASE_URL}/breeds`);
+export async function fetchBreeds() {
+  return await axios.get(`${BASE_URL}/breeds`);
 }
 
-export function fetchCatByBreeds(breedId) {
-  return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`);
+export async function fetchCatByBreeds(breedId) {
+  return await axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`);
 }
